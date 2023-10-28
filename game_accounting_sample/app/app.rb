ccec51af6a899ffdbb1da384419ab325a2ef48ee
tkhrk <>
2023-10-28 19:17:58 +0900

    add: credit event when game use

--- game_accounting_sample/app/app.rb
+++ game_accounting_sample/app/app.rb
@@ -6,6 +6,7 @@ $stdout.sync = true
 
 require 'pg'
 require 'kafka'
+require 'json'
 
 class Game
 
@@ -14,6 +15,8 @@ class Game
 
   def initialize
     @conn = PG.connect(host: "postgres", dbname: "game_db", user: "db_user", password: "db_password")
+    @kafka = Kafka.new(["game_accounting_sample-kafka-1:9092"])
+    @producer = @kafka.producer
   end
 
   def apply(user_id)
@@ -28,6 +31,12 @@ class Game
     current_timestamp = Time.now.strftime("%Y-%m-%d %H:%M:%S.%6N")
     uuid = SecureRandom.uuid
     res = @conn.exec("INSERT INTO game_usage (id, game_application_id, used_at, created_at, updated_at) VALUES ('#{uuid}', '#{game_application_id}', '#{current_timestamp}', '#{current_timestamp}', '#{current_timestamp}')")
+
+    puts "produce credit event"
+    # Produce a credit event to Kafka
+    credit_event = { action: "credit", game_application_id: game_application_id, timestamp: current_timestamp }.to_json
+    @producer.produce(credit_event, topic: "credits")
+    @producer.deliver_messages
   end
 
   def cancel_before_use(game_application_id)
